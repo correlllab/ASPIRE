0c0c0cc86ecb3e401cb620085e432987793b6c5d
James Watson <james.watson-2@colorado.edu>
2026-02-03 18:44:02 -0700

    sensible height restrictions

--- src/aspire/symbols.py
+++ src/aspire/symbols.py
@@ -57,7 +57,7 @@ def extract_position( obj_or_arr ) -> np.ndarray:
 def p_symbol_inside_workspace_bounds( obj_or_arr, noPad = False, addMargin = 0.0 ):
     """ Return True if inside the bounding box, Otherwise return False """
     posn = extract_position( obj_or_arr )      
-    pBox = (env_var("_MIN_X_OFFSET")-addMargin <= posn[0] <= env_var("_MAX_X_OFFSET")+addMargin) and (env_var("_MIN_Y_OFFSET")-addMargin <= posn[1] <= env_var("_MAX_Y_OFFSET")+addMargin) and (-0.65*env_var("_BLOCK_SCALE") < posn[2] <= env_var("_MAX_Z_BOUND"))
+    pBox = (env_var("_MIN_X_OFFSET")-addMargin <= posn[0] <= env_var("_MAX_X_OFFSET")+addMargin) and (env_var("_MIN_Y_OFFSET")-addMargin <= posn[1] <= env_var("_MAX_Y_OFFSET")+addMargin) and ( 0.25*env_var("_BLOCK_SCALE") < posn[2] <= env_var("_MAX_Z_BOUND"))
     if noPad:
         pFar = True
     else:
